45f37cbfd69c6c7090cb1f3a0ea09fe3fae17ef8
shreya-navalgund <68266432+shreya-navalgund@users.noreply.github.com>
2021-06-28 10:21:48 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>CopyRight by ShapeAI @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
